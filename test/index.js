eb36df448584af1a35b9f25472e58c39440e5d54
anzerr <thoron174+anzerr@gmail.com>
2020-08-14 02:34:58 +0200

    🚧 missing exit on test

--- test/index.js
+++ test/index.js
@@ -3,16 +3,10 @@ const {UserAgent} = require('../index'),
     data = require('./data'),
     {rule} = require('../src/rule');
 
-let count = 0;
 const u = new UserAgent();
 for (const i in data) {
     for (const x in data[i]) {
-        if (count < 10) {
-            u.get(data[i][x].user_agent);
-        } else {
-            break;
-        }
-        count++;
+        u.get(data[i][x].user_agent);
     }
 }
 
@@ -21,3 +15,5 @@ u.metric();
 
 const agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36';
 console.log(u.get(agent).data, agent);
+
+u.close();
