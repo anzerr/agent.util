4e67df1e4a70709007e475ff40ea4f347cbc84fd
anzerr <thoron174+anzerr@gmail.com>
2020-08-14 02:28:33 +0200

    🚧 dump

--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@ const u = new UserAgent();
 for (const i in data) {
     for (const x in data[i]) {
         if (count < 10) {
-            console.log(u.get(data[i][x].user_agent).data, data[i][x]);
+            u.get(data[i][x].user_agent);
         } else {
             break;
         }
@@ -17,7 +17,6 @@ for (const i in data) {
 }
 
 rule.sort();
-console.log(rule);
 u.metric();
 
 const agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36';
