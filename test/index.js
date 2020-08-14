505cd49a905950e5ea3eef543cad203b582c9c79
anzerr <thoron174+anzerr@gmail.com>
2020-08-14 02:25:53 +0200

    🚧 dump

--- test/index.js
+++ test/index.js
@@ -3,16 +3,22 @@ const {UserAgent} = require('../index'),
     data = require('./data'),
     {rule} = require('../src/rule');
 
+let count = 0;
 const u = new UserAgent();
 for (const i in data) {
     for (const x in data[i]) {
-        console.log(u.get(data[i][x].user_agent).data, data[i][x]);
+        if (count < 5) {
+            console.log(u.get(data[i][x].user_agent).data, data[i][x]);
+        } else {
+            u.get(data[i][x].user_agent)
+        }
+        count++;
     }
 }
 
 rule.sort();
 console.log(rule);
-console.log(u.metric());
+u.metric();
 
 const agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36';
 console.log(u.get(agent).data, agent);
